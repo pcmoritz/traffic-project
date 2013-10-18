a457c0c1faef02d776e200d4f3b9d1790e6a4418
Richard Shin <ricshin@cs.berkeley.edu>
2013-10-18 15:29:47 -0700

    Also compute mu in static_matrix.py, which contains the sum of the edge weights for the corresponding route in each alpha value.

--- static_matrix.py
+++ static_matrix.py
@@ -18,28 +18,36 @@ def generate_static_matrix(graph, routes, sensors, flow_portions, flow_from_each
   f = np.array([(graph[de[0]][de[1]]['flow'] if 'flow' in graph[de[0]][de[1]] else 0) for de in sensors])
   
   alphas = []
+  mus = []
   phis = []
   num_routes = []
   for node in graph.nodes():
     route_indices_from_node = route_indices_by_origin[node]
     edges_in_route = [set(zip(routes[i], routes[i][1:])) for i in route_indices_from_node]
     
-    alpha = np.zeros(shape=(len(route_indices_from_node),1))
+    alpha = np.zeros(shape=len(route_indices_from_node))
+    mu = np.zeros(shape=len(route_indices_from_node))
     phi = np.zeros(shape=(len(sensors), len(route_indices_from_node)))
     for j in xrange(len(route_indices_from_node)):
       alpha[j] = flow_portions[route_indices_from_node[j]]
+      route = routes[route_indices_from_node[j]]
+      mu[j] = sum(1./graph[de[0]][de[1]]['weight'] for de in zip(route, route[1:]))
+      
       for i in xrange(len(sensors)):
         if sensors[i] in edges_in_route[j]:
           phi[i, j] = flow_from_each_node
     
     num_routes.append(len(route_indices_from_node))
     phis.append(phi)
+    mus.append(mu)
     alphas.append(alpha)
-  return np.hstack(phis), np.concatenate(alphas), f, np.array(num_routes)
+  return np.hstack(phis), np.concatenate(alphas), np.concatenate(mus), f, np.array(num_routes)
   
 if __name__ == '__main__':
   graph, routes, sensors = small_graph.generate_small_graph()
   flow_portions = flows.annotate_with_flows(graph, routes)
   
-  phi, alpha, f, num_routes = generate_static_matrix(graph, routes, sensors, flow_portions)
-  scipy.io.savemat('small_graph.mat', {'phi': phi, 'alpha': alpha, 'f': f, 'num_routes': num_routes}, oned_as='column')
+  phi, alpha, mu, f, num_routes = generate_static_matrix(graph, routes, sensors, flow_portions)
+  scipy.io.savemat('small_graph.mat', {'phi': phi, 'alpha': alpha, 'mu': mu, 'f': f, 'num_routes': num_routes}, oned_as='column')
+  
+  print np.dot(phi, alpha) - f
