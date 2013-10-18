880175f2128ae0dddf7c4afd25a8c167f0ff81a7
Richard Shin <ricshin@cs.berkeley.edu>
2013-10-17 18:52:37 -0700

    Update flows to account for bidirectionality, and allocate flows per node instead of for the entire graph.

--- flows.py
+++ flows.py
@@ -7,7 +7,7 @@ import numpy.random
 
 import small_graph
 
-def annotate_with_flows(graph, routes, total_flow=1000.0, sparsity=0.01):
+def annotate_with_flows(graph, routes, flow_from_each_node=1.0, sparsity=0.01):
   '''Generate traffic from each origin onto some small fraction of its routes, and compute the amount of flow at each edge.'''
   
   # All route indices are with respect to _routes_.
@@ -15,7 +15,7 @@ def annotate_with_flows(graph, routes, total_flow=1000.0, sparsity=0.01):
   for i, route in enumerate(routes):
     route_indices_by_origin[route[0]].append(i)
   
-  flow_from_each_node = total_flow / len(graph)
+  flow_portions = [0] * len(routes)
   
   for node in graph.nodes():
     route_indices_from_node = route_indices_by_origin[node]
@@ -25,20 +25,29 @@ def annotate_with_flows(graph, routes, total_flow=1000.0, sparsity=0.01):
     selected_route_weights = numpy.random.dirichlet([1] * num_nonzero_routes, 1)[0]
     
     for i, w in zip(selected_route_indices, selected_route_weights):
+      flow_portions[i] = w
+      
       for u, v in zip(routes[i], routes[i][1:]):
         edge = graph.edge[u][v]
         current_flow = edge['flow'] if 'flow' in edge else 0
         edge['flow'] = current_flow + flow_from_each_node * w
+  
+  return flow_portions
           
 if __name__ == '__main__':
-  graph, routes = small_graph.generate_small_graph()
+  graph, routes, sensors = small_graph.generate_small_graph()
   annotate_with_flows(graph, routes)
 
   pos = nx.get_node_attributes(graph, 'pos')
   nx.draw(graph,pos)
   
-  edge_labels=dict([((u,v,),d['flow'] if 'flow' in d else 0)
-                    for u,v,d in graph.edges(data=True)])
+  edge_labels = collections.defaultdict(float)
+  for u, v, d in graph.edges(data=True):
+    if u > v:
+      u, v = v, u
+    edge_labels[(u, v)] += d['flow'] if 'flow' in d else 0
+#  edge_labels=dict([((u,v,),d['flow'] if 'flow' in d else 0)
+#                    for u,v,d in graph.edges(data=True)])
   nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels)
 
   matplotlib.pyplot.show()
