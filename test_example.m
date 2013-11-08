ebd935d178878a3dbbd17d8696180dc5aa70efb4
Cathy Wu <cathywu@alum.mit.edu>
2013-11-08 15:33:22 -0800

    more test

--- test_example.m
+++ test_example.m
@@ -2,6 +2,9 @@
 [errors comparisons] = small_sparse_recovery({'cvx_L2','cvx_raw',...
     'cvx_unconstrained_L1','cvx_weighted_L1'},'small_graph_OD');
 
+[errors comparisons] = small_sparse_recovery({'cvx_L2','cvx_raw',...
+    'cvx_unconstrained_L1','cvx_weighted_L1'},'small_graph');
+
 % Then to view the results test-by-test, select the test number, e.g.
 % errors(2)
 % comparison = comparisons(2);
