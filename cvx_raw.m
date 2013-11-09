6abfc240fed649eee5f38869cb5780a8eb5068e8
Richard Shin <ricshin@cs.berkeley.edu>
2013-11-08 16:16:45 -0800

    Add noiseless case to cvx_raw.m.

--- cvx_raw.m
+++ cvx_raw.m
@@ -1,12 +1,19 @@
 %% Raw objective (no regularization)
 function a = cvx_raw(p)
     Phi = p.Phi; f = p.f; n = p.n; L1 = p.L1; num_routes = p.num_routes;
+    noise = p.noise; epsilon = p.epsilon;
 
     cvx_begin quiet
         variable a(n)
-        variable t
-        minimize(square_pos(norm(Phi * a - f, 2)))
+        if ~noise
+            minimize 0
+        else
+            minimize(square_pos(norm(Phi * a - f, 2)))
+        end
         subject to
+        if ~noise
+            square_pos(norm(Phi * a - f, 2)) <= epsilon
+        end
         a >= 0
         L1 * a == ones(length(num_routes),1)
     cvx_end
