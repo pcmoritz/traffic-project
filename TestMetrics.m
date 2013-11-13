ab33d3b73d6d55200a692743f52f5a080bbd8a75
Cathy Wu <cathywu@alum.mit.edu>
2013-11-12 20:32:39 -0800

    test output and test metrics objects

---
+++ TestMetrics.m
@@ -0,0 +1,11 @@
+classdef TestMetrics < handle
+   properties (Access = public)
+       test_output
+       error_L1
+       error_L2
+       error_support
+       sparsity_diff
+   end
+   methods
+   end
+end
---
+++ TestOutput.m
@@ -0,0 +1,9 @@
+classdef TestOutput < handle
+   properties (Access = public)
+       test_parameters
+       comparisons % [real_a a]
+       matrix_type
+   end
+   methods
+   end
+end
