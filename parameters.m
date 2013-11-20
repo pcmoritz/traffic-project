d04e792e7279d8f70d52178562cb9f25c73e22de
Philipp Moritz <pcmoritz@gmail.com>
2013-11-19 22:06:25 -0800

    kind of finished the test metric serializing taks, wrote a small interface around test_example.m

--- parameters.m
+++ parameters.m
@@ -6,9 +6,11 @@ if strcmp(user,'cathywu') == 1
     raw_directory = './data/raw/';
     param_directory = './data/params/';
     output_directory = './data/output/';
+    metrics_directory = './data/metrics/';
 else
     python = 'LD_LIBRARY_PATH= python';
     raw_directory = '~/Dropbox/traffic/data/raw/';
     param_directory = '~/Dropbox/traffic/data/params/';
     output_directory = '~/Dropbox/traffic/data/output/';
+    metrics_directory = '~/Dropbox/traffic/data/metrics/';
 end
--- test_example.m
+++ test_example.m
@@ -1,57 +1,78 @@
-%% Sample test
-% clear all;
+% This function takes three parameters:
+% each parameter is 0 if flag is not set, 1 otherwise;
+% first parameter: shall the problems be generated?
+% second parameter: shall the output be generated?
+% third parameter: shall the metrics be generated?
 
-user = getenv('USER');
-if strcmp(user,'cathywu') == 1
-    python = '/opt/local/bin/python';
-else
-    python = 'LD_LIBRARY_PATH= python';
-end
+function test_example(varargin)
 
-rows = 5; cols = 5; k = 2; n = 2;
-command = sprintf(horzcat(['%s static_matrix.py --prefix "" --num_rows %d ', ...
-      '--num_cols %d --num_routes_per_od %d ', ...
-      '--num_nonzero_routes_per_o %d']), python, rows, cols, k, n)
-system(command);
+input = inputParser;
+input.FunctionName = 'TEST_EXAMPLE';
 
-p = TestParameters();
-p.rows = rows; p.cols = cols; p.nroutes = k; p.sparsity = 0;
-p.model_type = 'small_graph'; % 'small_graph_random', 'small_graph_OD'
+input.addOptional('generate_problems', 0, @isnumeric);
+input.addOptional('generate_output', 0, @isnumeric);
+input.addOptional('generate_metrics', 0, @isnumeric);
 
-[errors_L1 errors_L2 comparisons] = small_sparse_recovery(p,{'cvx_L2'});
+input.parse(varargin{:});
+
+generate_problems_p = input.Results.generate_problems;
+generate_output_p = input.Results.generate_output;
+generate_metrics_p = input.Results.generate_metrics;
+
+parameters;
+
+% rows = 5; cols = 5; k = 2; n = 2;
+% command = sprintf(horzcat(['%s static_matrix.py --prefix "" --num_rows %d ', ...
+%       '--num_cols %d --num_routes_per_od %d ', ...
+%       '--num_nonzero_routes_per_o %d']), python, rows, cols, k, n)
+% system(command);
+
+% p = TestParameters();
+% p.rows = rows; p.cols = cols; p.nroutes = k; p.sparsity = 0;
+% p.model_type = 'small_graph'; % 'small_graph_random', 'small_graph_OD'
+
+% [errors_L1 errors_L2 comparisons] = small_sparse_recovery(p,{'cvx_L2'});
 
 % TODO finish creating p object not via small_sparse_recovery
 % TODO save p to disk (O, OD, random, aug)
 
 tests = {'cvx_L2','cvx_raw','cvx_unconstrained_L1','cvx_weighted_L1'};
+
+if(generate_problems_p)
     
-options = {[4 3 10], [4 3 5]}; % The problems that will be generated
-generate_problem(options);
+    options = {[4 3 10], [4 3 5]}; % The problems that will be generated
+    generate_problem(options);
+end
 
-parameters % load names of the directories
+if(generate_output_p)
+    % load the problems from the directory
+    files = dir(fullfile(param_directory, '*.mat'));
+    for file = files'
+        numsamples = 1;
+        data = load(fullfile(param_directory, file.name));
+        output_list = generate_output(data.p, tests);
+        % display(output_list);
+        numsamples = save_test_outputs(output_directory, output_list, numsamples);
+    end
+end
 
-% load the problems from the directory
-files = dir(fullfile(param_directory, '*.mat'));
-for file = files'
+if(generate_metrics_p)
     numsamples = 1;
-    data = load(fullfile(param_directory, file.name));
-    output_list = generate_output(data.p, tests);
-    % display(output_list);
-    numsamples = save_test_outputs(output_directory, output_list, numsamples);
+
+    files = dir(fullfile(output_directory, '*.mat'));
+    for file = files'
+        data = load(fullfile(output_directory, file.name));
+        m = output_to_metrics(data.o);
+        filename = sprintf('TestMetrics-%s-%s-%d', user, datestr(now, 30), numsamples);
+        save(strcat(metrics_directory, filename), 'm');
+        numsamples = numsamples + 1;
+    end
 end
 
-% TODO load some p object
-
-for test = tests
-    o = TestOutput();
-    o.test_parameters = p; o.algorithm = test{1}; o.tester = getenv('USER');
-    run_algorithm_test(o);
-    % TODO save this object to disk
-    
-    m = output_to_metrics(o);
-end
 %%
 % Then to view the results test-by-test, select the test number, e.g.
 % errors(2)
 % comparison = comparisons(2);
 % comparison{1}
+
+end
