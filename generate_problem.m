1986952c9defe64f251a092a48fa6c5e2cde6cd1
Cathy Wu <cathywu@alum.mit.edu>
2013-11-19 19:12:01 -0800

    write testparams object to disk by timestamp

--- generate_problem.m
+++ generate_problem.m
@@ -11,8 +11,6 @@
 % what_to_generate maps algorithms to options
 
 % options = {[4 3 10], [4 3 5]};
-% what_to_generate = containers.Map({'cvx_L2', 'cvx_raw', 'cvx_weighted_L1'}, ...
-%     {options, options, options});
 
 % files written to disk: contains TestOutput
 
@@ -20,6 +18,7 @@ function generate_problem(options)
 %GENERATE_OUTPUT Convert python data files into "raw" algorithm input
 
 methods = {'O', 'OD', 'random'};
+models = {'small_graph_random', 'small_graph_OD', 'small_graph'};
 
 date = datestr(now, 30);
 
@@ -28,31 +27,42 @@ numsamples = 1;
 user = getenv('USER');
 if strcmp(user,'cathywu') == 1
     python = '/opt/local/bin/python';
+    raw_directory = './data/raw/';
+    param_directory = './data/params/';
 else
     python = 'LD_LIBRARY_PATH= python';
+    raw_directory = '~/Dropbox/traffic/data/raw/';
+    param_directory = '~/Dropbox/traffic/data/params/';
 end
 
 for option = options
-        vec = option{1};
-        rows = vec(1);
-        cols = vec(2);
-        k = vec(3);
-        directory = '~/Dropbox/traffic/data/raw/';
-        subdir = char(strcat(num2str(rows), '_', num2str(cols), ...
-            '_', num2str(k), '_', num2str(date), '_', num2str(numsamples)));
-        
-        command = sprintf('%s static_matrix.py --prefix %s%s_ --num_rows %d --num_cols %d --num_routes_per_od %d', python, ...
-            directory, subdir, rows, cols, k);
-        numsamples = numsamples + 1;
-        fprintf('Generating "raw" for %s\n', subdir);
-        system(command);
-        
-        directory = '~/Dropbox/traffic/data/problems/';
-        fprintf('Generating "problem" for %s\n', subdir);
-        
-        for method = methods
-            create_dir(char(strcat(directory, method)));
-        end
+    vec = option{1};
+    rows = vec(1);
+    cols = vec(2);
+    k = vec(3);
+    subdir = char(strcat(num2str(rows), '_', num2str(cols), ...
+        '_', num2str(k), '_', num2str(date), '_', num2str(numsamples)));
+
+    command = sprintf('%s static_matrix.py --prefix %s%s_ --num_rows %d --num_cols %d --num_routes_per_od %d', python, ...
+        raw_directory, subdir, rows, cols, k);
+    numsamples = numsamples + 1;
+    fprintf('Generating "raw" for %s\n', subdir);
+    system(command);
+
+    fprintf('Generating "parameters" for %s\n', subdir);
+    for model=models
+        filename = sprintf('%s/%s_%s',raw_directory,subdir,model{1});
+        p = TestParameters();
+        p.rows = rows; p.cols = cols; p.nroutes = k; p.sparsity = 0;
+        p.model_type = model{1};
+        model_to_testparameters(p,filename);
+        save(sprintf('%s/%s-%s',param_directory,datestr(now, 30),getenv('USER')),'p');
+        % TODO save p to disk by user-timestamp
+    end
+    
+    for method = methods
+        create_dir(char(strcat(param_directory, method)));
+    end
 end
 
 end
