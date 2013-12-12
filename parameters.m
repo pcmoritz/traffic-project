01e5fb2c0be85da8506f060ee788b16a5a68d740
Cathy Wu <cathywu@alum.mit.edu>
2013-12-11 16:14:34 -0800

    test on larger matrices with fewer constraints

--- parameters.m
+++ parameters.m
@@ -1,4 +1,6 @@
 % The parameters for the file system structure, etc.
+mode = 'REAL'; % DEBUG, REAL
+
 user = getenv('USER');
 addpath 'lbfgsb-matlab/src';
 if strcmp(user,'cathywu') == 1
@@ -15,8 +17,6 @@ else
     addpath '~/mosek/7/toolbox/r2009b';
 end
 
-mode = 'REAL'; % DEBUG, REAL
-
 raw_directory = [base_directory, 'raw/'];
 param_directory = [base_directory, 'params/'];
 output_directory = [base_directory, 'output/'];
@@ -25,8 +25,7 @@ graphs_directory = [base_directory, 'graphs/'];
 
 tests = {'cvx_L2','cvx_raw','cvx_unconstrained_L1','cvx_weighted_L1', ...
     'cvx_hot_start_lp', 'cvx_block_descent_L_infty', ...
-    'cvx_random_sample_L_infty'};
-% tests = {'cvx_random_sample_L_infty_hot_start'};
+    'cvx_random_sample_L_infty_hot_start'};
 
 % Library with names for the different parameters/settings/algorithms
 max_sparsity = .5;
@@ -82,9 +81,9 @@ sparsity_sizes = [sparsity_values', sparsity_values' + 0.05];
 %% Random matrix
 matrix_sizes('random') = [];
 
-for no_constraints=10:15
-    for no_blocks = 2:3
-        for no_vars_per_block = 10:12
+for no_constraints=5:2:15
+    for no_blocks = 2:4
+        for no_vars_per_block = 10:2:12
             for sparsity = sparsity_values
                 % Spars stands for the number of nonzero routes you choose
                 % at each origin
