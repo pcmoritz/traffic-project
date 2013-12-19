feac73c3741da3c73d539f5b67fd32e1617ba645
Cathy Wu <cathywu@alum.mit.edu>
2013-12-18 23:15:34 -0800

    add parameter to repeat generating problems

--- test_example.m
+++ test_example.m
@@ -42,7 +42,7 @@ if(generate_problems_p)
     %delete(fullfile(param_directory, '*.mat'));
 
     generate_problem('traffic', matrix_sizes('traffic'));
-    generate_problem('random', matrix_sizes('random'));
+    generate_problem('random', matrix_sizes('random'),2);
 end
 
 if(generate_output_p)
